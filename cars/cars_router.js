59fc0300a8c16bdac5367a5ef8114c4b1b461896
janecyyu <janecyyu@gmail.com>
2020-05-12 14:00:53 -0700

    add seed

--- cars/cars_router.js
+++ cars/cars_router.js
@@ -3,3 +3,32 @@ const express = require("express");
 
 const db = require("../data/dbConnection");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  db("car-dealer")//table name
+    .then((cars) => {
+      res.json(cars);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to retrieve cars" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const carData = req.body;
+  db("car-dealer")
+    .insert(carData)
+    .then((ids) => {
+      db("car-dealer")
+        .where({ id: ids[0] })
+        .then((newCar) => {
+          res.status(201).json(newCar);
+        });
+    })
+    .catch((err) => {
+      console.log("POST error", err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+});
+
+module.exports = router;
--- migrations/20200512125033_create-cars-table.js
+++ migrations/20200512125033_create-cars-table.js
@@ -1,15 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable("car-dealer", (tbl) => {
     //primary key, called id, auto increments, not null
-    // tbl.increments();
+    tbl.increments();
     tbl.string("VIN", 17).notNullable().unique();
     tbl.string("make").notNullable();
     tbl.string("model").notNullable();
-    tbl.string("mileage").notNullable();
+    tbl.integer("mileage").notNullable();
     tbl.string("transmission type");
     tbl.string("status");
-
-    // tbl.timestamps(true, true);
   });
 };
 
